c40b11df6d2b9b0421642e861afe3c816974dab5
solutionsgeek <mmcgillis@gmail.com>
2015-09-03 22:58:14 -0700

    CG listing!
    auth fixed, added CG searching

--- rpauto.py
+++ rpauto.py
@@ -8,5 +8,19 @@ password="admin"
 rpip="192.168.128.128"
 congrp="ERP"
 #test to check RPA state
-response=requests.get("http://%s/fapi/rest/4_0/state/rpas/all" % rpip, auth=(user,password))
-print (response)
+#response=requests.get("http://%s/fapi/rest/4_0/state/rpas/all" % rpip, auth=(user,password))
+#print (response)
+#response=requests.post("http://%s/fapi/rest/4_0/settings/groups/actions/create_bookmarks",params=)
+
+#find the guid of the consistency group we want
+r=requests.get('https://192.168.128.128/fapi/rest/4_0/settings/groups/all',auth=('admin','admin'), verify=False)
+#turn the response text into json and search it for the CG we want
+search_r=json.loads(r.text)
+count = 0
+while search_r[count]['name'] != congrp:
+  print (search_r[count]['name'])
+  count += 1
+
+cg_guid = search_r[count]['groupUID']['id']
+##just to be sure!
+print (cg_guid, search_r[count]['name'])
