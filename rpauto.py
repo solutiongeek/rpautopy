dcbc1c722c83a2576fdd416626fc9fc17f0e2a81
solutionsgeek <mmcgillis@gmail.com>
2015-09-04 00:28:22 -0700

    added some stuff that doesn't work
    post request to create bookmark inserted but broke. it's a placeholder

--- rpauto.py
+++ rpauto.py
@@ -2,6 +2,8 @@
 # Bookmarks an RP CG and puts it in image access mode so that it can be snapped
 import requests
 import json
+import datetime
+from datetime import datetime
 # Phase 1 - Authentication (user/password/ip/CG are defaults for RP Simulator)
 user="admin"
 password="admin"
@@ -24,3 +26,11 @@ while search_r[count]['name'] != congrp:
 cg_guid = search_r[count]['groupUID']['id']
 ##just to be sure!
 print (cg_guid, search_r[count]['name'])
+
+#now to create a bookmark!
+bmark_stamp=str(datetime.now())
+bmark_name=congrp+'_'+bmark_stamp
+#I don't like spaces
+bmark_name=bmark_name.replace(" ",'')
+# This command is terrible and doesn't work. Pass JSON params. fix soon.
+r=requests.post('https://192.168.128.128/fapi/rest/4_0/settings/groups/actions/create_bookmark',auth=('admin','admin'), verify=False, groups=(cg_guid),bookmarkname=bmark_name)
